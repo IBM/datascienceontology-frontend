3a4f78c078a9533fb19ff75ff0df1548c650e77b
epatters <ejpatters@gmail.com>
2017-07-10 22:56:39 -0400

    Add skeleton for TypeScript/React page.

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+export interface HelloProps { compiler: string; framework: string; }
+
+export const Hello = (props: HelloProps) =>
+  <h1>Hello from {props.compiler} and {props.framework}!</h1>;
+
+ReactDOM.render(
+  <Hello compiler="TypeScript" framework="React" />,
+  document.getElementById("main")
+);
