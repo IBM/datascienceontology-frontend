39d1c5d05f679b4f066e37fcb87988766d04ed66
epatters <ejpatters@gmail.com>
2017-07-12 11:09:28 -0400

    Fetch list of concepts from CouchDB database using PouchDB.

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,58 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import * as PouchDB from "pouchdb";
+import * as PouchDBFind from 'pouchdb-find';
+PouchDB.plugin(PouchDBFind);
 
-export interface HelloProps { compiler: string; framework: string; }
+interface IConceptName {
+  ontology: string;
+  id: string;
+  name: string;
+}
 
-export const Hello = (props: HelloProps) =>
-  <h1>Hello from {props.compiler} and {props.framework}!</h1>;
+interface IAppProps {
+  db: PouchDB.Database;
+  ontology: string;
+}
+interface IAppState {
+  concepts: Array<IConceptName>;
+}
+
+export class App extends React.Component<IAppProps,IAppState> {
+  constructor(props: IAppProps) {
+    super(props);
+    this.state = {
+      concepts: []
+    }
+  }
+  
+  componentWillMount() {
+    this.props.db.find({
+      selector: {
+        schema: "concept",
+        ontology: this.props.ontology
+      },
+      fields: [ "ontology", "id", "name" ]
+    }).then(result => {
+      this.setState({concepts: result.docs as Array<any>});
+    });
+  }
+  
+  render() {
+    return (
+      <div className="concept-list">
+        <ul>
+          {this.state.concepts.map(concept =>
+            <li key={concept.id}>{concept.name}</li>)}
+        </ul>
+      </div>
+    );
+  }
+}
+
+const db = new PouchDB("***REMOVED***/data-science-ontology");
 
 ReactDOM.render(
-  <Hello compiler="TypeScript" framework="React" />,
+  <App db={db} ontology="data-science" />,
   document.getElementById("main")
 );
