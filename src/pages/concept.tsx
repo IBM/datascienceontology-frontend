cdd9941a16ad9f56e3efd5905ca892abbb6ae020
epatters <ejpatters@gmail.com>
2017-07-30 22:52:06 -0400

    Display domain and codomain on morphism concept page.

--- src/pages/concept.tsx
+++ src/pages/concept.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import * as Router from "react-router-dom";
 
-import { Concept } from "data-science-ontology";
+import { Concept, MorphismConcept, DomainObject } from "data-science-ontology";
 import * as Services from "../services";
 
 import "../../style/pages/concept.css";
@@ -51,6 +51,9 @@ export const ConceptDisplay = (props: {concept: Concept}) => {
         </li>)}
       </ul>
     </div>;
+  const kindDisplay = concept.kind == "morphism" ?
+    <MorphismDisplay concept={concept as MorphismConcept} /> : null;
+  
   return (
     <div className="concept">
       <h3>
@@ -59,7 +62,7 @@ export const ConceptDisplay = (props: {concept: Concept}) => {
         </span>
         {concept.name}
       </h3>
-      <dl className="dl-horizontal">
+      <dl className="dl-horizontal" style={{marginBottom: 0}}>
         <dt>ID</dt>
         <dd>{concept.id}</dd>
         <dt>Kind</dt>
@@ -71,6 +74,38 @@ export const ConceptDisplay = (props: {concept: Concept}) => {
         {superconcepts && <dt>Is</dt>}
         {superconcepts && <dd>{superconcepts}</dd>}
       </dl>
+      {kindDisplay}
     </div>
   );
+}
+
+const MorphismDisplay = (props: {concept: MorphismConcept}) => {
+  const concept = props.concept;
+  return <dl className="dl-horizontal">
+    <dt>Domain</dt>
+    <dd><DomainObjectsDisplay objects={concept.domain} /></dd>
+    <dt>Codomain</dt>
+    <dd><DomainObjectsDisplay objects={concept.codomain} /></dd>
+  </dl>;
+}
+
+const DomainObjectsDisplay = (props: {objects: DomainObject[]}) => {
+  return <div className="domain-object-list">
+    <ol>{props.objects.map((object,i) =>
+      <li key={i}>
+        <DomainObjectDisplay object={object} />
+      </li>)}
+    </ol>
+  </div>;
+}
+
+const DomainObjectDisplay = (props: {object: DomainObject}) => {
+  const obj = props.object;
+  return <div className="domain-object">
+    {obj.name !== undefined && `${obj.name}: `}
+    <Router.Link to={`/concept/${obj.object}`}>{obj.object}</Router.Link>
+    {obj.description !== undefined && <p className="text-muted">
+      {obj.description}
+    </p>}
+  </div>;
 }
