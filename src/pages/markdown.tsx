d713200632b8f95ec1fcb848a8db97cae24f6383
epatters <ejpatters@gmail.com>
2017-12-06 14:03:14 -0600

    WIP: Render S-expressions in Markdown documents using SExpComponent.

--- src/pages/markdown.tsx
+++ src/pages/markdown.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
 import * as Router from "react-router-dom";
 
+import { SExp } from "open-discovery";
 import { MarkdownDocument } from "open-discovery-components";
+import { SExpComponent } from "../components/sexp";
 
 
 type MarkdownPageProps = Router.RouteComponentProps<{page: string}>;
@@ -9,5 +11,12 @@ type MarkdownPageProps = Router.RouteComponentProps<{page: string}>;
 export const MarkdownPage = (props: MarkdownPageProps) => {
   const page = props.match.params.page;
   const pageURL = `/assets/pages/${page}.md`;
-  return <MarkdownDocument docURL={pageURL} />;
+  return <MarkdownDocument docURL={pageURL} options={{
+    renderers: {
+      sexp: (props: {value: string, children: string[]}) => {
+        const sexp = JSON.parse(props.children[0]) as SExp;
+        return <SExpComponent sexp={sexp} />;
+      }
+    },
+  }} />;
 }
