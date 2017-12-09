f9fdd1555b3ad88d327014245fad8b02c980283f
epatters <ejpatters@gmail.com>
2017-12-09 13:18:05 -0600

    Add renderer for Cytoscape figures in Markdown documents.

--- src/pages/markdown.tsx
+++ src/pages/markdown.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import * as Router from "react-router-dom";
 
 import { SExp } from "open-discovery";
-import { MarkdownDocument } from "open-discovery-components";
+import { CytoscapeDocument, MarkdownDocument } from "open-discovery-components";
 import { SExpComponent } from "../components/sexp";
 
 import "../../style/pages/markdown.css";
@@ -15,6 +15,10 @@ export const MarkdownPage = (props: MarkdownPageProps) => {
   const pageURL = `/assets/pages/${page}.md`;
   return <MarkdownDocument docURL={pageURL} options={{
     renderers: {
+      cytoscape: (props: {value: string, children: string[]}) => {
+        const docURL = `/assets/pages/${props.value}.json`;
+        return <p><CytoscapeDocument docURL={docURL} /></p>;
+      },
       sexp: (props: {value: string, children: string[]}) => {
         const sexp = JSON.parse(props.children[0]) as SExp;
         return <p><SExpComponent sexp={sexp} /></p>;
