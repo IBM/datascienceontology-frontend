c57993c0dae48e0c8a2fdd50aa417cd72123ea2a
epatters <ejpatters@gmail.com>
2017-12-09 17:39:45 -0600

    Apply custom style to Cytoscape graphs in Markdown documents.

--- src/pages/markdown.tsx
+++ src/pages/markdown.tsx
@@ -6,6 +6,7 @@ import { CytoscapeDocument, MarkdownDocument } from "open-discovery-components";
 import { SExpComponent } from "../components/sexp";
 
 import "../../style/pages/markdown.css";
+import * as CytoscapeStyle from "../../style/cytoscape.json";
 
 
 type MarkdownPageProps = Router.RouteComponentProps<{page: string}>;
@@ -17,7 +18,16 @@ export const MarkdownPage = (props: MarkdownPageProps) => {
     renderers: {
       cytoscape: (props: {value: string, children: string[]}) => {
         const docURL = `/assets/pages/${props.value}.json`;
-        return <p><CytoscapeDocument docURL={docURL} /></p>;
+        return <p>
+          <CytoscapeDocument docURL={docURL}
+            defaults={{
+              style: CytoscapeStyle as any,
+              autolock: true,
+              userPanningEnabled: false,
+              userZoomingEnabled: false,
+            }}
+          />
+        </p>;
       },
       sexp: (props: {value: string, children: string[]}) => {
         const sexp = JSON.parse(props.children[0]) as SExp;
