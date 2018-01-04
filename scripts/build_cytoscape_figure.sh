1cce340babf717dce924e52138afaaa0e2361f0f
epatters <ejpatters@gmail.com>
2018-01-04 12:00:07 -0600

    Use raw output in script to build Cytoscape figures.
    Requires Cloud Functions Shell v1.3.374:
    
    https://github.com/ibm-functions/shell/issues/122

--- scripts/build_cytoscape_figure.sh
+++ scripts/build_cytoscape_figure.sh
@@ -5,11 +5,5 @@ if [[ $# -eq 0 ]] ; then
   echo 'Missing argument: JSON file'; exit 1
 fi
 
-# XXX: Work around pretty-printed but invalid JSON output from fsh by
-# extracting stringified JSON with regex.
-out=$(fsh app invoke data-science-ontology/build_cytoscape_figure -P "$1")
-if [[ "$out" =~ value:\ \"(.*)\" ]] ; then
-  echo "${BASH_REMATCH[1]}" | jq '.' > "$1"
-else
-  echo 'Failed to parse output'; exit 1
-fi
+result=$(fsh app invoke --raw-output data-science-ontology/build_cytoscape_figure -P "$1")
+echo "$result" | jq '.' > "$1"
